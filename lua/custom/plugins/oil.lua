3c06b6050315dc7210f1b32a0025a5cb53fbc055
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-05-31 20:47:25 -0700

    properly disable oil.lua

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,8 +1,11 @@
 -- Currently disabled because it doesn't play nice with cmake-tools.lua
 -- Too use together, you need to nvim directly into CMakeLists.txt or else it breaks: 'nvim CMakeLists.txt'
+
 -- return {
 --   'stevearc/oil.nvim',
 --   opts = {},
 --   -- Optional dependencies
 --   dependencies = { 'nvim-tree/nvim-web-devicons' },
 -- }
+
+return {}
