06772bf94aa524341b70bf7cb25f5d394d7b4e33
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-06-30 19:27:28 -0700

    test

--- init.lua
+++ init.lua
@@ -104,5 +104,6 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- test
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
