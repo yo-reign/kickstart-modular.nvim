a7720df616647411e0a8ea121d8221b40fde5e67
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-11-05 20:18:56 -0800

    Update Zellij plugin for better interoperobility

--- lua/custom/plugins/zellij.lua
+++ lua/custom/plugins/zellij.lua
@@ -1,20 +1,20 @@
 return {
-  'Lilja/zellij.nvim',
-  lazy = true,
-  event = 'VeryLazy',
-  keys = {
-    { '<M-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
-    { '<M-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
-    { '<M-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
-    { '<M-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
-  },
+  'https://github.com/fresh2dev/zellij.vim.git',
+  lazy = false,
+  --event = 'VeryLazy',
+  -- keys = {
+  --   { '<M-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
+  --   { '<M-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+  --   { '<M-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+  --   { '<M-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
+  -- },
   opts = {},
-  --config = function()
-  --  require('zellij').setup {}
+  config = function()
+    --  require('zellij').setup {}
 
-  --vim.api.nvim_set_keymap('n', '<M-h>', ':ZellijNavigateLeft<CR>', { noremap = true, silent = true })
-  --vim.api.nvim_set_keymap('n', '<M-j>', ':ZellijNavigateDown<CR>', { noremap = true, silent = true })
-  --vim.api.nvim_set_keymap('n', '<M-k>', ':ZellijNavigateUp<CR>', { noremap = true, silent = true })
-  --vim.api.nvim_set_keymap('n', '<M-l>', ':ZellijNavigateRight<CR>', { noremap = true, silent = true })
-  --end,
+    vim.api.nvim_set_keymap('n', '<M-h>', ':ZellijNavigateLeft!<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<M-j>', ':ZellijNavigateDown<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<M-k>', ':ZellijNavigateUp<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<M-l>', ':ZellijNavigateRight!<CR>', { noremap = true, silent = true })
+  end,
 }
