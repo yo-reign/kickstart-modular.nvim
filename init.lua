46d754d259e899ce727ffaf569917b243f685c77
yo-reign <danielaguiar0006@gmail.com>
2024-12-29 23:29:36 -0800

    add rainbow delimters plugin

--- init.lua
+++ init.lua
@@ -106,7 +106,7 @@ require("lazy-bootstrap")
 require("lazy-plugins")
 
 -- Set default colorscheme
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme tokyonight")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
