65b317f90ce051a3f6641fdc2fb6f5e8ce88148e
yo-reign <danielaguiar0006@gmail.com>
2025-04-18 21:16:33 -0700

    change avante setting

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -51,7 +51,7 @@ return {
 			auto_suggestions = false, -- Experimental stage
 			auto_set_highlight_group = true,
 			auto_set_keymaps = true,
-			auto_apply_diff_after_generation = false,
+			auto_apply_diff_after_generation = true,
 			support_paste_from_clipboard = false,
 			minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
 			enable_token_counting = true, -- Whether to enable token counting. Default to true.
