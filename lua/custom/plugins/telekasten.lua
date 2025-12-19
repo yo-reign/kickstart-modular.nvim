5deda0a5c4237a530616e7cb5c64bd6f0b466c29
yo-reign <danielaguiar0006@gmail.com>
2025-12-18 18:50:38 -0800

    add new keybinds for telekasten

--- lua/custom/plugins/telekasten.lua
+++ lua/custom/plugins/telekasten.lua
@@ -5,5 +5,26 @@ return {
 		require("telekasten").setup({
 			home = vim.fn.expand("~/zettelkasten"),
 		})
+
+		vim.keymap.set("n", "<leader>zkp", ":Telekasten panel<CR>", { desc = "Open telekasten panel" })
+		vim.keymap.set("n", "<leader>zkfg", ":Telekasten search_notes<CR>", { desc = "Grep inside zettelkasten notes" })
+		vim.keymap.set(
+			"n",
+			"<leader>zkn",
+			":Telekasten new_templated_note<CR>",
+			{ desc = "Create new templated zettelkasten note" }
+		)
+		vim.keymap.set("n", "<leader>zkff", ":Telekasten find_notes<CR>", { desc = "Find zettelkasten notes" })
+		vim.keymap.set(
+			"n",
+			"<leader>zkfr",
+			":Telekasten find_friends<CR>",
+			{ desc = "Find related (friend) zettelkasten notes" }
+		)
+		vim.keymap.set("n", "<leader>zkfl", ":Telekasten follow_link<CR>", { desc = "Follow telekasten link" })
+		vim.keymap.set("n", "<leader>zkbl", ":Telekasten show_backlinks<CR>", { desc = "Show telekasten backlinks" })
+		vim.keymap.set("n", "<leader>zkt", ":Telekasten show_tags<CR>", { desc = "Show telekasten tags" })
+		vim.keymap.set("n", "<leader>zkc", ":Telekasten show_calendar<CR>", { desc = "Show telekasten calendar" })
+		vim.keymap.set("n", "<leader>zky", ":Telekasten yank_notelink<CR>", { desc = "Yank telekasten notelink" })
 	end,
 }
