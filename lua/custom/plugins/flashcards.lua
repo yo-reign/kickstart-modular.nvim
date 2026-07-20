bd4f47644e2deea490ead6432fb0f893190bab94
REIGN <danielaguiar0006@gmail.com>
2026-07-20 12:11:22 -0700

    feat: enable flashcard media dependencies

--- lua/custom/plugins/flashcards.lua
+++ lua/custom/plugins/flashcards.lua
@@ -4,11 +4,27 @@ return {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		"nvim-telescope/telescope.nvim",
+		{
+			"3rd/image.nvim",
+			build = false, -- magick_cli uses the Homebrew ImageMagick executable
+			opts = {
+				backend = "kitty", -- Ghostty supports the Kitty graphics protocol
+				processor = "magick_cli",
+				-- Flashcards uses image.nvim's direct API and owns float cleanup.
+				integrations = {
+					markdown = { enabled = false },
+				},
+			},
+		},
 	},
 	config = function()
 		require("flashcards").setup({
 			directories = { "~/notes/" },
 			db_path = "~/notes/assets/",
+			media = {
+				enabled = true,
+				roots = { "~/notes/" },
+			},
 			fsrs = {
 				target_correctness = 0.95,
 			},
